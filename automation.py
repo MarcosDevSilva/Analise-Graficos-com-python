5e09e1e6ba207daf033b14a6e397ef1ceac46a74
Marcos <tassio50000@gmail.com>
2024-06-10 15:55:32 -0300

    finalizando o projetinho

--- automation.py
+++ automation.py
@@ -1,5 +1,33 @@
 import pandas
+import plotly.express as px
 
 dados = pandas.read_excel("vendas.xlsx")
 
-print(dados)
+# estatisticas
+# dados.describe()
+
+# analises
+dados["loja"].value_counts()
+
+# agrupando dados
+#print(dados.groupby("loja")["preco"].sum())
+
+# agrupando dados, em mais de uma coluna
+dados_agrupado = dados.groupby(["loja", "cidade", "estado", "forma_pagamento"])["preco"].sum()
+
+# criando arquivo excel
+dados_agrupado.to_excel("Fatoramento.xlsx")
+
+
+
+
+lista_colunas = ["loja", "cidade", "estado", "tamanho", "local_consumo"]
+
+for coluna in lista_colunas:
+    graficos = px.histogram(dados, x=coluna,
+                            y="preco",
+                            text_auto=True,
+                            title="Faturamento",
+                            color="forma_pagamento")
+    graficos.show()
+    graficos.write_html(f"Faturamento-{coluna}.html")
